62036c97f7b25a9dc05411c7553337aa74a54c1b
Tanvi Rastogi <tanvi.rastogi892@gmail.com>
2019-04-02 10:35:30 +0530

    Adding login page

--- src/app/index.js
+++ src/app/index.js
@@ -25,7 +25,6 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
 }
-console.log('window: ', window);
 
 var routes = {
     'register': register,
@@ -38,3 +37,39 @@ function onNavItemClick(section) {
     content.innerHTML = routes[section];
 }
 
+(function ($) {
+    "use strict";
+
+    debugger;
+    /*==================================================================
+    [ Focus input ]*/
+    $('.input100').each(function(){
+        $(this).on('blur', function(){
+            if($(this).val().trim() != "") {
+                $(this).addClass('has-val');
+            }
+            else {
+                $(this).removeClass('has-val');
+            }
+        })    
+    });
+
+    var showPass = 0;
+    $('.btn-show-pass').on('click', function(){
+        if(showPass == 0) {
+            $(this).next('input').attr('type','text');
+            $(this).find('i').removeClass('zmdi-eye');
+            $(this).find('i').addClass('zmdi-eye-off');
+            showPass = 1;
+        }
+        else {
+            $(this).next('input').attr('type','password');
+            $(this).find('i').addClass('zmdi-eye');
+            $(this).find('i').removeClass('zmdi-eye-off');
+            showPass = 0;
+        }
+        
+    });
+
+
+})(jQuery);
--- src/app/login/login.js
+++ src/app/login/login.js
@@ -1,3 +1,17 @@
-var login = `<div>
-    Hello from login.html
-</div>`
+var login = `
+<div class="wrap-login100">
+    <form class="login100-form validate-form"></form>
+    <div class="wrap-input100 validate-input" data-validate = "Valid email is: a@b.c">
+        <input class="input100" type="text" name="email">
+        <span class="focus-input100" data-placeholder="Email"></span>
+    </div>
+
+    <div class="wrap-input100 validate-input" data-validate="Enter password">
+        <span class="btn-show-pass">
+            <i class="zmdi zmdi-eye"></i>
+        </span>
+        <input class="input100" type="password" name="pass">
+        <span class="focus-input100" data-placeholder="Password"></span>
+    </div>
+</div>
+`
