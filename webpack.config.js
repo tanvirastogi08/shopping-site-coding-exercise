7f1092ff17c56620e2cda62611e4a3881e78ea68
Tanvi Rastogi <tanrasto@publicisgroupe.net>
2019-04-10 10:36:04 +0530

    using const instead of var

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-var CopyWebpackPlugin = require('copy-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: ['@babel/polyfill', './src/app/index.js'],
